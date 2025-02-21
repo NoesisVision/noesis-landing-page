65e536fc5f09ecd33d4523f4d6919a6394ed0075
lszydlo <szydler@gmail.com>
2025-02-21 20:05:39 +0100

    Add GH pages deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind()],
+  site: 'https://lszydlo.github.io',
+  base: '/noesis-landing-page',
 });
