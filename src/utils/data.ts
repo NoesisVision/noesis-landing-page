734e41768844f5f802b438e940eb004b71b4cef8
szjanikowski <szymon.janikowski@itlibrium.com>
2025-02-27 17:23:23 +0100

    Perspectives and small tweaks

--- src/utils/data.ts
+++ src/utils/data.ts
@@ -1,17 +1,17 @@
 const services = [
     {
         title: "Domain perspective",
-        description: "Immediately see processes and use cases of your system and their relationships with deeper building blocks like services, data repositories, DDD patterns and external APIs.",
+        description: "Gain instant visibility into your system’s processes and use cases, along with their connections to deeper building blocks such as services, data repositories, Domain-Driven Design (DDD) patterns, and external APIs.",
         icon: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6"> <path stroke-linecap="round" stroke-linejoin="round" d="M12 2l9 5v10l-9 5-9-5V7l9-5z" /> </svg>`,
     },
     {
         title: "Technology perspective",
-        description: "Browse tech elements on different level: deployable units (monoliths, microservices), dbclusters and even packages or classes. See how they connect to elements from other perspectives e.g. domain building blocks",
+        description: "Explore your system’s technical structure across multiple levels—from deployable units like monoliths and microservices to database clusters, packages, and even individual classes. Understand how these elements interconnect with domain building blocks and other perspectives.",
         icon: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6"> <path stroke-linecap="round" stroke-linejoin="round" d="M10.5 6l-6 6 6 6M13.5 6l6 6-6 6" /> </svg>`,
     },
     {
         title: "People perspective",
-        description: "Identify people and teams responsible for development of respective parts of the other perspectives - see who develops a given module in a monolith or a microservice.",
+        description: "Identify the teams and individuals responsible for different aspects of your system. See exactly who develops specific modules within a monolith or microservice, ensuring clarity in ownership and collaboration.",
         icon: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6"> <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 9A3.75 3.75 0 1112 5.25 3.75 3.75 0 0115.75 9zM4.5 18.75a8.25 8.25 0 0115 0" /> </svg>`,
     }
 ];
